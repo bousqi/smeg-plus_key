82269b65e1e515c07a9b730ccc67a238071d9803
Bousqi <bousqi@gmail.com>
2020-01-18 23:20:43 +0000

    missing sudo on getting rpi-source

--- build_eem.sh
+++ build_eem.sh
@@ -5,7 +5,7 @@ sudo apt install bison flex libssl-dev ssl-cert
 # check rpi-source
 if [ -z "$(which rpi-source)" ]; then
 	sudo apt install bc
-	wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source && sudo chmod +x /usr/bin/rpi-source && /usr/bin/rpi-source -q --tag-update
+	sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source && sudo chmod +x /usr/bin/rpi-source && /usr/bin/rpi-source -q --tag-update
 fi
 
 # around 150 Mo
